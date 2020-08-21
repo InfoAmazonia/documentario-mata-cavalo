0e2d7c112eaace480cca232a4a740532dc491750
Paulo Eduardo Campos Afonso <pauloeduardocamposafonso@Akhenaton.local>
2020-08-21 12:51:05 -0300

    first commit

---
+++ src/components/Head.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { injectIntl, intlShape, defineMessages } from "react-intl";
+import { Helmet } from "react-helmet";
+
+const messages = defineMessages({
+  title: {
+    id: "head.title",
+    defaultMessage: "Documentário “O Mata Cavalo”"
+  },
+  description: {
+    id: "head.description",
+    defaultMessage:
+      "Um documentário sobre a origem do território herdado por remanescentes de quilombolas no Mato Grosso"
+  },
+  keywords: {
+    id: "head.keywords",
+    defaultMessage: "mata cavalo, quilombola"
+  }
+});
+
+class Head extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { intl, location } = this.props;
+    const title = intl.formatMessage(messages.title);
+    const description = intl.formatMessage(messages.description);
+    const keywords = intl.formatMessage(messages.keywords);
+    const url = process.env.SITE_URL || "";
+    return (
+      <Helmet>
+        <html lang={intl.locale} />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <link rel="canonical" href={`${url}${location.pathname}`} />
+        <meta property="og:url" content={`${url}${location.pathname}`} />
+        <meta
+          property="og:image"
+          content={`${url}${require("images/socialmedia.jpg")}`}
+        />
+        <meta property="og:title" content="{title}" />
+        <meta property="og:description" content="{description}" />
+        <meta property="og:type" content="video.movie" />
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:image:src" content={`${url}${require("images/socialmedia.jpg")}`}/>
+        <meta name="twitter:site" content="@amazonia_real"/>
+        <meta name="twitter:domain" content="Amazônia Real"/>
+        <meta name="twitter:creator" content="@amazonia_real"/>
+      </Helmet>
+    );
+  }
+}
+
+Head.propTypes = {
+  intl: intlShape.isRequired
+};
+
+export default injectIntl(withRouter(Head));
---
+++ src/components/containers/VideoEndContent.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+import RelatedNews from "components/RelatedNews";
+import { Link } from "react-router-dom";
+import { FormattedMessage } from "react-intl";
+
+const Wrapper = styled.div`
+// background: lightgray;
+.video-ending--section {
+}
+.link-back {
+  text-align: center;
+  a {
+    font-family: 'Merriweather', serif;
+    font-size: 0.5em;
+    letter-spacing: 0.1rem;
+    display: inline-block;
+    color: #fff;
+    border: 1px solid #fff;
+    text-align: center;
+    margin: -1px -1px 0 0;
+    padding: 0.75rem 1rem;
+    font-weight: 600;
+    width: 210px;
+    text-transform: uppercase;
+  }
+}
+`
+
+var divStyle = {
+  'zIndex':'99999',
+};
+
+var divStyle2 = {
+  'position': 'fixed',
+  'right': '0',
+  'top': '200px',
+  'minWidth': '100%',
+  'minHeight': '100%'
+};
+const VideoContentEnd = ({ data }) => (
+  <Wrapper className='video-ending--section' style={divStyle}>
+    <div style={divStyle2}>
+      <span>{data}</span>
+      <RelatedNews className='related-alternative'
+          sectionTitle={"Confira também: "}
+          title={"A luta dos herdeiros de Mata Cavalo pelo título do quilombo"}
+          subtitle={"O território foi formado no século 19, quando negros africanos compraram ou ganharam as terras após o fim da escravidão no Brasil. Agora enfrentam dificuldades para aderir o Cadastro Ambiental Rural (CAR)" }
+          imageUrl={require("images/matacavalo_related.jpg")}
+          href={"//matacavalo.amazoniareal.com.br/"}
+        />
+        <div className="link-back">
+          <Link to="/">
+            <FormattedMessage
+              id="general.backToHome"
+              defaultMessage="Começar do início"
+            />
+          </Link>
+        </div>
+    </div>
+  </Wrapper>
+)
+
+export default VideoContentEnd
